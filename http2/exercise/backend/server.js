9660db2a8422c6a9ec7d60c61dc46118c43a5eb9
BShamm <bashambrook@gmail.com>
2023-10-05 15:22:54 +0100

    http2 implementation

--- http2/exercise/backend/server.js
+++ http2/exercise/backend/server.js
@@ -29,11 +29,29 @@ const server = http2.createSecureServer({
   key: fs.readFileSync(path.join(__dirname, "/../key.pem")),
 });
 
-/*
- *
- * Code goes here
- *
- */
+server.on("stream", (stream, headers) => {
+  const path = headers[":path"];
+  const method = headers[":method"];
+
+  // stream open for every request from the browser
+  if (path === "/msgs" && method === "GET") {
+    // immediately reply with 200 OK and the encoding
+    console.log("connected a stream", +stream.id);
+    stream.respond({
+      ":status": 200,
+      "content-type": "text/plain; charset=utf-8",
+    });
+
+    // write the first response
+    stream.write(JSON.stringify({ msg: getMsgs() }));
+    connections.push(stream);
+
+    stream.on("close", () => {
+      console.log("disconnected " + stream.id);
+      connections = connections.filter((s) => s !== stream);
+    });
+  }
+});
 
 server.on("request", async (req, res) => {
   const path = req.headers[":path"];
@@ -53,11 +71,17 @@ server.on("request", async (req, res) => {
     const data = Buffer.concat(buffers).toString();
     const { user, text } = JSON.parse(data);
 
-    /*
-     *
-     * some code goes here
-     *
-     */
+    msg.push({
+      user,
+      text,
+      time: Date.now(),
+    });
+
+    res.end();
+
+    connections.forEach((stream) => {
+      stream.write(JSON.stringify({ msg: getMsgs() }));
+    });
   }
 });
 
